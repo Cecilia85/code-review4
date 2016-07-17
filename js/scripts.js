138b4c7f8893960c3dee549487b2ea1e84620d08
Cecilia Le Brech <cecilialebrch@gmail.com>
2016-07-16 21:54:10 -0700

    change js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 //business logic
-function Pizza(name,toppings,size, quantity,price) {
+function Pizza(name,toppings,size, quantity) {
   this.name= name;
   this.toppings= toppings;
   this.size= size;
   this.quantity= quantity;
-  this.price= price;
+
 }
 
 Pizza.prototype.price = function() {
-  var result;
-  if (this.size==="small"){
-    result= this.price*2;
-  }else if (this.size==="medium"){
-    result = this.price*3;
-  }else if (this.size==="large"){
-    result= this.price*4;
+  var pizzaPrice;
+  if (this.size === "small"){
+    pizzaprice * 2;
+  }else if (this.size === "medium"){
+    pizzaPrice * 3;
+  }else if (this.size === "large"){
+    pizzaPrice * 4;
   };
 
-  return result* this.quantity;
+  return (pizzaPrice * this.quantity);
   };
 // user interface logic
 $(document).ready(function() {
@@ -26,12 +26,15 @@ $(document).ready(function() {
 
     var inputtedName = $("input#new-name").val();
     var selectedToppings1= $("select#new-toppings1").val();
-    var selectedToppings2= $("select#newtoppings2").val();
+    var selectedToppings2= $("select#new-toppings2").val();
     var selectedSize= $("select#size").val();
-    var inputtedQuantity= $("input#quantity").val();
+    var inputtedQuantity= parseInt($("input#quantity").val());
     var newPrice= new Pizza(inputtedName,selectedToppings1, selectedToppings2,selectedSize,inputtedQuantity);
 
 
+
+
+    $(".prices").last().click(function() {
     $("#pizza").show();
     $(".name").text(newPrice.name);
     $(".quantity").text(newPrice.quantity);
@@ -40,3 +43,4 @@ $(document).ready(function() {
 });
 
   });
+});
